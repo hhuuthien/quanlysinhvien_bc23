10300b338fa6e5df9e3ff9a2607a4478c8debe87
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-03-04 20:49:05 +0700

    devA-code-tinh-diem-trung-binh

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -1,11 +1,14 @@
 function SinhVien() {
-    this.maSinhVien = '';
-    this.tenSinhVien = '';
-    this.loaiSinhVien = '';
-    this.diemRenLuyen = '';
-    this.email ='';
-    this.soDienThoai ='';
-    this.diemToan = '';
-    this.diemLy = '';
-    this.diemHoa = '';
+  this.maSinhVien = "";
+  this.tenSinhVien = "";
+  this.loaiSinhVien = "";
+  this.diemRenLuyen = "";
+  this.email = "";
+  this.soDienThoai = "";
+  this.diemToan = "";
+  this.diemLy = "";
+  this.diemHoa = "";
+  this.tinhDiemTrungBinh = function () {
+    console.log("Tính điểm trung bình");
+  };
 }
