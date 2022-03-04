4c41161fdba3887a79115e5ac49f507092cafc0c
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-03-04 21:14:12 +0700

    devA moi nhat

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -1,16 +1,16 @@
 function SinhVien() {
-    this.maSinhVien = '';
-    this.tenSinhVien = '';
-    this.loaiSinhVien = '';
-    this.diemRenLuyen = '';
-    this.email ='';
-    this.soDienThoai ='';
-    this.diemToan = '';
-    this.diemLy = '';
-    this.diemHoa = '';
-    this.xepLoai = function (){
-        console.log('dev b coe hàm xếp loại');
-    }
+  this.maSinhVien = "";
+  this.tenSinhVien = "";
+  this.loaiSinhVien = "";
+  this.diemRenLuyen = "";
+  this.email = "";
+  this.soDienThoai = "";
+  this.diemToan = "";
+  this.diemLy = "";
+  this.diemHoa = "";
+  this.xepLoai = function () {
+    console.log("dev b coe hàm xếp loại");
+  };
   this.tinhDiemTrungBinh = function () {
     console.log("Tính điểm trung bình");
   };
